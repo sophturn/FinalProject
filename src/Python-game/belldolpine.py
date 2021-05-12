7baf698cc41a37aa8839e01fcdf5e5f8d935c2c9
sophturn <70330883+sophturn@users.noreply.github.com>
2021-05-12 07:49:22 -0600

    Add files via upload

---
+++ src/Python-game/belldolpine.py
@@ -0,0 +1,14 @@
+import random
+class Belldolpine:
+  #member variables
+  hp = 10
+  exp = 0
+  #constructor
+
+  #methods
+  def attack1(self):
+    damage = int(random(7,9))
+  def attack2(self):
+    damage = int(random(7,9))
+  def heal(self):
+    regain = int(random(3,5))
